f73c83e55fdf75c5a47e79d7a5a55a8981b125c4
javedproeffico <javed@proeffico.com>
2025-11-13 15:49:54 +0530

    app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Sahi kaam kiya h "
+    return "Sahi kaam kiya h very good"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
