344660a971d2265ee872cd5049e6dca94b2ad350
javedproeffico <javed@proeffico.com>
2025-11-13 15:06:28 +0530

    app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Javedd Docker CI/CD Pipeline working! Yeahhhhhh Greate"
+    return "Javedd Docker CI/CD Pipeline working! Yeahhhhhh Greate sabbass"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
